ea6328b3e4760f7429d13023326cfa5ea4594899
ReubenKabiti <reubenrk2@gmail.com>
2025-01-16 11:54:23 +0100

    feat: implement sdl window

--- src/core/window/sdl_window/sdl_window.cpp
+++ src/core/window/sdl_window/sdl_window.cpp
@@ -2,18 +2,65 @@
 
 namespace UC
 {
+  SDLWindow::~SDLWindow()
+  {
+    SDL_DestroyWindow(m_window);
+    SDL_Quit();
+  }
+
   bool SDLWindow::init()
   {
-    return false;
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+      SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL initialization failed: %s\n", SDL_GetError());
+      return false;
+    }
+    m_window = SDL_CreateWindow(
+        getTitle().data(),
+        getWidth(),
+        getHeight(),
+        SDL_WINDOW_RESIZABLE);
+
+    if (m_window == NULL)
+    {
+      SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+      SDL_Quit();
+      return false;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(m_window, NULL);
+    if (renderer == NULL)
+    {
+      SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create renderer: %s\n", SDL_GetError());
+      SDL_DestroyWindow(m_window);
+      SDL_Quit();
+      return false;
+    }
+
+    return true;
   }
   void SDLWindow::clear(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0)
   {
+    SDL_SetRenderDrawColor(m_renderer, r, g, b, 255);
+    SDL_RenderClear(m_renderer);
   }
   void SDLWindow::update()
   {
+    SDL_RenderPresent(m_renderer);
   }
   std::queue<WindowEvent> SDLWindow::pollEvents()
   {
-    return std::queue<WindowEvent>();
+    std::queue<WindowEvent> windowEvents;
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+      case SDL_EVENT_QUIT:
+        windowEvents.push(WindowEvent(EventType::WINDOW_CLOSE));
+        break;
+      }
+    }
+    return windowEvents;
   }
 }
--- src/core/window/sdl_window/sdl_window.hpp
+++ src/core/window/sdl_window/sdl_window.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <SDL3/SDL.h>
 #include "../window.hpp"
 
 namespace UC
@@ -6,9 +7,16 @@ namespace UC
   class SDLWindow : public Window
   {
   public:
+    ~SDLWindow();
     bool init() override;
     void clear(uint8_t r, uint8_t g, uint8_t b) override;
     void update() override;
     std::queue<WindowEvent> pollEvents() override;
+
+    inline SDL_Window *getSDLWindow() { return m_window; }
+
+  private:
+    SDL_Window *m_window;
+    SDL_Renderer *m_renderer;
   };
 }
